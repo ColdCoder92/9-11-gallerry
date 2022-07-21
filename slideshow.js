2373c1aad7a6da5189335bc86b690ecf442d8681
Lucas Rodriguez <lucasrodriguez0831@gmail.com>
2022-07-21 10:54:32 -0400

    Update Header, Footer, & Slideshow

--- slideshow.js
+++ slideshow.js
@@ -1,10 +1,4 @@
-slideshow();
-
-function slideshow(){
-    let leftArrow = document.getElementById("leftArrow");
-    let rightArrow = document.getElementById("rightArrow");
-    let imgMain = document.getElementById("main");
-    let imgOffset = document.getElementById("offset");
+$(function() {
     let photos = ["images/stock1.jpg", "images/stock2.jpg"];
     var index = 0;
 
@@ -12,52 +6,88 @@ function slideshow(){
     forward();
 
     function back() {
-        leftArrow.addEventListener("click", function() {
+        $("#leftArrow").on("click", function() {
             if (index != 0){
-                imgMain.style.animationName = "back";
-                imgOffset.style.animationName = "backwardAppear";
-                animationToggle();
                 index--;    
-                assign();
-            }    
+                $("#main").css("animation-name", "back");
+                $("#offset").css("animation-name", "backwardAppear");
+            } 
+            else {
+                index = photos.length - 1;
+                $("#offset").css("animation-name", "forwardAppear");
+                $("#main").css("animation-name", "forward");
+            }
+            animationToggle();   
+        });
+        $("#leftArrow").on("click", function() {
+            assign();
         });
     }
 
     function forward() {
-        rightArrow.addEventListener("click", function() {
-            if (index != photos.length - 1){
-                imgMain.style.animationName = "forward";
-                imgOffset.style.animationName = "forwardAppear";
+        manual();
+        auto();
+        function manual() {
+            $("#rightArrow").on("click", function() {
+                if (index != photos.length - 1){
+                    index++;  
+                    $("#main").css("animation-name", "forward");
+                    $("#offset").css("animation-name", "forwardAppear");
+                }
+                else {
+                    index = 0;
+                    $("#offset").css("animation-name", "backwardAppear");
+                    $("#main").css("animation-name", "back");
+                }    
                 animationToggle();
-                index++;  
-                assign();  
-            }    
-        });
+            });
+            $("#rightArrow").on("click", function() {
+                assign();
+            });
+        }
+        function auto() {
+            setInterval(function() {
+                if (index != photos.length - 1){
+                    index++;  
+                    assign();
+                    $("#main").css("animation-name", "forward");
+                    $("#offset").css("animation-name", "forwardAppear");
+                }
+                else {
+                    index = 0;
+                    assign();
+                    $("#offset").css("animation-name", "backwardAppear");
+                    $("#main").css("animation-name", "back");
+                }    
+                animationToggle();    
+            }, 3000);
+        }
     }
 
-    function animationToggle(){
-        if (imgMain.style.animationPlayState == "paused" 
-        && imgOffset.style.animationPlayState == "paused"){
-            imgMain.style.animationPlayState = "running";
-            imgOffset.style.animationPlayState = "running";
+    function animationToggle() {
+        if ($("#main").css("animation-play-state") == "paused" 
+        && $("#offset").css("animation-play-state") == "paused"){
+            $("#main").css("animation-play-state", "running");
+            $("#offset").css("animation-play-state", "running");
         }
          else {
-            imgMain.style.animationPlayState = "paused";
-            imgOffset.style.animationPlayState = "paused";
+            $("#main").css("animation-play-state", "paused");
+            $("#offset").css("animation-play-state", "paused");
         }
         setTimeout(function() {
-            imgMain.style.animationPlayState = "paused";
-            imgOffset.style.animationPlayState = "paused";
+            $("#main").css("animation-play-state", "paused");
+            $("#offset").css("animation-play-state", "paused");
         }, 1000);
     }
 
-    function assign(){
-        imgMain.setAttribute("src", photos[index]);
+    function assign() {
+        $("#offset").attr("src", photos[index]);
         if (index == 0){
-            imgOffset.setAttribute("src", photos[index + 1]);
+            $("#main").attr("src", photos[index + 1]);
         }
         else {
-            imgOffset.setAttribute("src", photos[index - 1]);
+            $("#main").attr("src", photos[index - 1]);
         }
+        console.log("Main Index: " + index);
     }
-}
+});
